2af5edfd5c2ce47db4ab73e975201be5177308be
Christopher Hankins <Chankins5050@gmail.com>
2022-01-05 17:10:04 -0500

    fixed structural issues with html and added logic to my js file

--- assets/script.js
+++ assets/script.js
@@ -19,5 +19,26 @@ function startQuiz() {
 
 //Get current question and choices
 function getCurrentQuestion() {
-    var currentQuestion = questions[currentQuestionIndex]
+    var currentQuestion = questions[currentQuestionIndex];
+    var titleElement = document.querySelector("#question-title");
+    titleElement.textContent = currentQuestion.title;
+    questionChoices.textContent = "";
+
+    for (var i=0; i < currentQuestion.choice.length; i++) {
+        var choiceNode = document.createElement("button");
+        choiceNode.setAttribute("class", "choice");
+        choiceNode.setAttribute("value", currentQuestion.choice[i]);
+
+        questionChoices.appendChild(choiceNode);
+    }
 }
+
+//All my questions for the quiz
+var questions = [
+    {
+        title: ""
+    }
+]
+
+//Add event listener for click to start the quiz
+startBtn.addEventListener("click", startQuiz);
